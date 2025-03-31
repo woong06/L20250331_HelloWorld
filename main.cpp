75893c6e648d0a62f9e741f15ab4a359001a5506
woong06 <seongung7431@gmail.com>
2025-03-31 17:00:13 +0900

    산술연산자 추가
    + - * / % 값을 구하고
    float형으로 형변환을 해서 소수점까지 출력

--- main.cpp
+++ main.cpp
@@ -10,5 +10,17 @@ int main()
 	float A = 5.5f;
 	int B = (int)A;
 	
+
+	int Number3 = Number1 + Number2;
+	cout << Number3 << endl;
+	int Number4 = Number1 - Number2;
+	cout << Number4 << endl;
+	int Number5 = Number1 * Number2;
+	cout << Number5 << endl;
+	float Number6 = (float)Number1 / (float)Number2;
+	cout << Number6 << endl;
+	int Number7 = Number1 % Number2;
+	cout << Number7 << endl;
+
 	return 0;
 }
