9776762768ab116c92d1f4170c2943de04082866
woong06 <seongung7431@gmail.com>
2025-04-01 09:09:26 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main()
 	int Number1 = 5;
 	int Number2 = 4;
 
-	float A = 5.5f;
-	int B = (int)A;
+	float Num1 = 5.5f;
+	float Num2 = 4.4f;
 	
 
 	int Number3 = Number1 + Number2;
@@ -17,10 +17,21 @@ int main()
 	cout << Number4 << endl;
 	int Number5 = Number1 * Number2;
 	cout << Number5 << endl;
-	float Number6 = (float)Number1 / (float)Number2;
+	int Number6 = (float)Number1 / (float)Number2;
 	cout << Number6 << endl;
 	int Number7 = Number1 % Number2;
 	cout << Number7 << endl;
+
+	float Num3 = Num1 + Num2;
+	cout << Num3 << endl;
+	float Num4 = Num1 - Num2;
+	cout << Num4 << endl;
+	float Num5 = Num1 * Num2;
+	cout << Num5 << endl;
+	float Num6 = Num1 / Num2;
+	cout << Num6 << endl;
+	float Num7 = (int)Num1 % (int)Num2;
+	cout << Num7 << endl;
 
 	return 0;
 }
